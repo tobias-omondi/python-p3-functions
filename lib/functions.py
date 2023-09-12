0bdc23f3df3097ac1d4b0ca78428f00f5e87c5ea
tobias <tobias.omondi@student.moringaschool.com>
2023-09-12 15:19:43 +0300

    work-done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,44 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+
+    return number / 2
+
+# example of the test
+
+# fuction myFunction (params) { 
+# console.log("running myFunction")
+# return param +1;
+# }
+
+# def my_function(promp):
+#     print("running my_function")
+#     return promp+ 1
+
+# def say_hi(name):
+#     print(f"Hi there, {name}!")
+
+# say_hi()
+
+# def stylish_painter():
+#     best_hairstyle = "Bob Ross"
+#     return "Jean-Michel Basquiat"
+#     return best_hairstyle
+#     print(best_hairstyle)
+
+# stylish_painter()
